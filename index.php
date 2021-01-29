01edb92b19263b8654bd91f2c7680601741ef697
hilmireza09 <h.rezamuhammad@gmail.com>
2021-01-29 18:57:12 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, obcaecati! Dolore, rerum! Tempora quod obcaecati excepturi, repellat iusto, consequatur eaque facilis repudiandae qui cumque accusamus numquam, odio iure vitae sed!</p>
 </body>
 </html>
